d2e78893038f6df1a492f41f4cedea82e03e7372
1348580915@qq.com <18161931993@163.com>
2019-08-26 10:02:12 +0800

    add two method to solve TwoSum problem

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -1,7 +1,8 @@
 package easy;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author bx
@@ -20,9 +21,12 @@ import java.util.Arrays;
  */
 public class TwoSum {
     /**
-     * return a result
+     * Approach 1: Brute Force
+     * @param nums int array
+     * @param target target value
+     * @return Two array index
      */
-    private static int[] twoSum(int[] nums, int target) {
+    private static int[] twoSum1(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) {
@@ -33,10 +37,48 @@ public class TwoSum {
         throw new IllegalArgumentException("No two sum solution");
     }
 
+    /**
+     * Approach 2: Two-pass Hash Table
+     * @param nums int array
+     * @param target target value
+     * @return Two array index
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(16);
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * Approach 3: One-pass Hash Table
+     * @param nums int array
+     * @param target target value
+     * @return Two array index
+     */
+    public static int[] twoSum3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(16);
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5};
-        int target = 0;
-        int[] result = twoSum(array, target);
+        int target = 5;
+        int[] result = twoSum3(array, target);
         System.out.println(Arrays.toString(result));
     }
 }
