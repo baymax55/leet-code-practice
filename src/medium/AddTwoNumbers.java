69f779d5ff33cced7b851fcf698c2a49eb1f59c5
1348580915@qq.com <18161931993@163.com>
2019-08-27 09:59:59 +0800

    add AddTwoNumbers

---
+++ src/medium/AddTwoNumbers.java
@@ -0,0 +1,96 @@
+package medium;
+
+
+import java.util.Arrays;
+
+/**
+ * @author bx
+ * @date 8/26/2019 5:19 PM
+ * <p>
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse
+ * order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * <p>
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * <p>
+ * Example:
+ * <p>
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ */
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
+
+/**
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ * Example:
+ *
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ */
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(2);
+        ListNode node2= new ListNode(4);
+        ListNode node3= new ListNode(3);
+        node1.next=node2;
+        node2.next=node3;
+        
+        ListNode node7 = new ListNode(5);
+        ListNode node8= new ListNode(6);
+        ListNode node9= new ListNode(4);
+        node7.next=node8;
+        node8.next=node9;
+
+        ListNode result = addTwoNumbers(node1, node7);
+        System.out.println(result.toString());
+    }
+
+    /**
+     * Approach 1: Elementary Math
+     * @param l1 ListNode1
+     * @param l2 ListNode2
+     * @return ListNode
+     */
+    private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+}
+
