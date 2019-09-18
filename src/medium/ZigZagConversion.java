6562ea8275a7248a113dbafa8c887cea9ae405c1
bx <18161931993@163.com>
2019-09-18 15:24:34 +0800

    add ZigZag Conversion

---
+++ src/medium/ZigZagConversion.java
@@ -0,0 +1,64 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author bx
+ * @date 9/18/2019 3:10 PM
+ * <p>
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+ * <p>
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * <p>
+ * Write the code that will take a string and make this conversion given a number of rows:
+ * <p>
+ * string convert(string s, int numRows);
+ * Example 1:
+ * <p>
+ * Input: s = "PAYPALISHIRING", numRows = 3
+ * Output: "PAHNAPLSIIGYIR"
+ * Example 2:
+ * <p>
+ * Input: s = "PAYPALISHIRING", numRows = 4
+ * Output: "PINALSIGYAHRPI"
+ * Explanation:
+ * <p>
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ */
+public class ZigZagConversion {
+    public static void main(String[] args) {
+        String tempString="PAYPALISHIRING";
+//        int tempInt=3;
+        int tempInt=4;
+        System.out.println(convert(tempString,tempInt));
+    }
+
+    private static String convert(String s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++)
+            rows.add(new StringBuilder());
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for (StringBuilder row : rows) ret.append(row);
+        return ret.toString();
+    }
+}
